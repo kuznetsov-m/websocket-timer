d9286606e406474430eef007ee67a147938689fc
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2022-07-14 16:41:56 +0300

    Add timer button

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from threading import Lock
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, emit
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
@@ -9,20 +13,28 @@ socketio = SocketIO(app)
 thread = None
 thread_lock = Lock()
 
+isTimerStarted = False
+
 def background_thread():
     TIMER_INTERVAL = 0.5
     value = 0
     while True:
         socketio.sleep(TIMER_INTERVAL)
-        value += TIMER_INTERVAL
         socketio.emit(
             'my_response',
             {'data': 'Server timer event', 'timer': value}
         )
+        value = value + TIMER_INTERVAL if isTimerStarted else 0
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/toggle', methods=['POST'])
+def stop():
+    global isTimerStarted
+    isTimerStarted = not isTimerStarted
+    return jsonify()
 
 @socketio.event
 def my_ping():
