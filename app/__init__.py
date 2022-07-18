fd8139faf358471527d73cc235b8a77ee11afcb1
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2022-07-18 20:15:15 +0300

    Update transport polling -> websocket

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import eventlet
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-socketio = SocketIO(app)
+async_mode = 'eventlet'
+socketio = SocketIO(app, transports=['websocket', 'polling'], async_mode=async_mode, cors_allowed_origins="*", \
+    # logger=True, engineio_logger=True
+)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
